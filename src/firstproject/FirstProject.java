7ca3c8ad509d9d602e4f8d9c561e85b025461d58
Haman-lab <mundiham@sheridancolleg.ca>
2024-09-16 12:52:04 -0400

    add comment

---
+++ src/firstproject/FirstProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstproject;
+
+/**
+ *
+ * @author hamanmundi
+ * @modifiedby hamanmundi
+ */
+public class FirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+        // TODO code application logic here
+    }
+    
+}
